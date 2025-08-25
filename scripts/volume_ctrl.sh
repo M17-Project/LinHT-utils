f2908771749b1817af2fef46ef9969b1ae77757b
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2025-08-25 15:22:41 +0200

    Add files via upload

---
+++ scripts/volume_ctrl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+b=100 #log base
+
+while true
+do
+    vol=$(< /sys/bus/iio/devices/iio:device0/in_voltage0_raw)
+    vol=$(echo "170 + l(1+($vol/3000)*($b-1))/l($b) * (255-170) + 1" | bc -l)
+    vol=$(printf "%.0f" "$vol")
+    amixer -q -c wm8960audio set Playback $vol
+    #echo $vol
+    sleep 0.2
+done
