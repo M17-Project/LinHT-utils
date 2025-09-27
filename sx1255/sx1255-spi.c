03408dfef4624a28df53258e918d5b07e3ac2d3d
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2025-09-27 11:24:24 +0300

    use libsx1255

--- sx1255/sx1255-spi.c
+++ sx1255/sx1255-spi.c
@@ -1,41 +1,19 @@
+// Build with gcc sx1255-spi.c -o sx1255-spi -lm -lsx1255
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
 #include <math.h>
 #include <errno.h>
-#include <sys/ioctl.h>
-#include <linux/spi/spidev.h>
-#include <gpiod.h>
 #include <getopt.h>
 
-// Build with gcc sx1255-spi.c -o sx1255-spi -lgpiod -lm
+#include <sx1255.h>
 
 // --- Configuration ---
-#define CLK_FREQ (32.0e6f)
-#define RST_PIN_OFFSET 22 // GPIO pin 22 on gpiochip0 for reset
-
+const uint16_t rst_pin_offset = 22;
 const char *spi_device = "/dev/spidev0.0";
 const char *gpio_chip_path = "/dev/gpiochip0"; // Use device path for gpiod V2
-
-// --- Global libgpiod handles ---
-struct gpiod_chip *chip_v2;
-struct gpiod_line_request *rst_line_request;
-
-// --- Unchanged SPI and SX1255 Functions (Omitted for brevity) ---
-uint8_t bits = 8;
-uint32_t speed = 500000;
-uint8_t mode;
-
-// --- SX1255 ---
-typedef enum
-{
-    RATE_125K,
-    RATE_250K,
-    RATE_500K
-} rate_t;
 
 // settings
 uint32_t rxf;
@@ -45,261 +23,9 @@ int8_t dac_gain;
 uint8_t lna_gain;
 uint8_t pga_gain;
 uint16_t pll_bw;
-rate_t rate;
+sx1255_rate_t rate;
 uint8_t addr;
 uint8_t val;
-
-int spi_init(char *dev)
-{
-    int fd;
-    int ret;
-
-    fd = open(dev, O_RDWR);
-    if (fd < 0)
-    {
-        perror("Can't open SPI device");
-        return fd;
-    }
-
-    ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-    if (ret == -1)
-    {
-        perror("Can't set SPI write mode");
-        close(fd);
-        return -1;
-    }
-    ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
-    if (ret == -1)
-    {
-        perror("Can't set SPI read mode");
-        close(fd);
-        return -1;
-    }
-
-    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-    if (ret == -1)
-    {
-        perror("Can't set SPI write bits per word");
-        close(fd);
-        return -1;
-    }
-    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-    if (ret == -1)
-    {
-        perror("Can't set SPI read bits per word");
-        close(fd);
-        return -1;
-    }
-
-    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-    if (ret == -1)
-    {
-        perror("Can't set SPI write max speed");
-        close(fd);
-        return -1;
-    }
-    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-    if (ret == -1)
-    {
-        perror("Can't set SPI read max speed");
-        close(fd);
-        return -1;
-    }
-
-    close(fd);
-    return 0;
-}
-
-void sx1255_writereg(uint8_t addr, uint8_t val)
-{
-    int fd = open(spi_device, O_RDWR);
-    if (fd < 0)
-    {
-        perror("sx1255_writereg: open");
-        return;
-    }
-    uint8_t tx[2] = {addr | (1 << 7), val};
-    struct spi_ioc_transfer tr = {
-        .tx_buf = (unsigned long)tx,
-        .rx_buf = 0,
-        .len = 2,
-    };
-    ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-    close(fd);
-    usleep(10000U);
-}
-
-uint8_t sx1255_readreg(uint8_t addr)
-{
-    int fd = open(spi_device, O_RDWR);
-    if (fd < 0)
-    {
-        perror("sx1255_readreg: open");
-        return 0;
-    }
-    uint8_t tx[2] = {addr & ~(1 << 7), 0};
-    uint8_t rx[2] = {0, 0};
-    struct spi_ioc_transfer tr = {
-        .tx_buf = (unsigned long)tx,
-        .rx_buf = (unsigned long)rx,
-        .len = 2,
-    };
-    ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-    close(fd);
-    usleep(10000U);
-    return rx[1];
-}
-
-void sx1255_set_rx_freq(uint32_t freq)
-{
-    uint32_t val = lround((float)freq * 1048576.0f / CLK_FREQ);
-    sx1255_writereg(0x01, (val >> 16) & 0xFF);
-    sx1255_writereg(0x02, (val >> 8) & 0xFF);
-    sx1255_writereg(0x03, val & 0xFF);
-}
-
-void sx1255_set_tx_freq(uint32_t freq)
-{
-    uint32_t val = lround((float)freq * 1048576.0f / CLK_FREQ);
-    sx1255_writereg(0x04, (val >> 16) & 0xFF);
-    sx1255_writereg(0x05, (val >> 8) & 0xFF);
-    sx1255_writereg(0x06, val & 0xFF);
-}
-
-int8_t sx1255_setrate(rate_t r)
-{
-    uint8_t n, div;
-
-    switch (r)
-    {
-    case RATE_125K:
-        n = 5;
-        div = 2;
-        break;
-
-    case RATE_250K:
-        n = 4;
-        div = 1;
-        break;
-
-    case RATE_500K:
-        n = 3;
-        div = 0;
-        break;
-
-    default: // 125k
-        n = 5;
-        div = 2;
-        break;
-    }
-
-    // interpolation/decimation factor = mant * 3^m * 2^n
-    sx1255_writereg(0x13, (0 << 7) | (0 << 6) | (n << 3) | (1 << 2));
-
-    // mode B2, XTAL/CLK_OUT division factor=2^div
-    sx1255_writereg(0x12, (2 << 4) | div);
-
-    // return i2s status flag
-    return (sx1255_readreg(0x13) & (1 << 1)) >> 1;
-}
-
-int gpio_init(void)
-{
-    // Open the GPIO chip
-    chip_v2 = gpiod_chip_open(gpio_chip_path);
-    if (!chip_v2)
-    {
-        fprintf(stderr, "Error opening GPIO chip '%s': %s\n", gpio_chip_path, strerror(errno));
-        return -1;
-    }
-
-    // Create line settings object to configure individual lines
-    struct gpiod_line_settings *settings = gpiod_line_settings_new();
-    if (!settings)
-    {
-        perror("Error creating line settings");
-        gpiod_chip_close(chip_v2);
-        return -1;
-    }
-    // Configure the settings for our output pin
-    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
-    gpiod_line_settings_set_output_value(settings, 0); // Initial value is LOW
-
-    // Create a line config object to hold the settings for lines
-    struct gpiod_line_config *line_cfg = gpiod_line_config_new();
-    if (!line_cfg)
-    {
-        perror("Error creating line config");
-        gpiod_line_settings_free(settings);
-        gpiod_chip_close(chip_v2);
-        return -1;
-    }
-    // Add the settings for our specific line (by offset) to the config
-    unsigned int offsets[] = {RST_PIN_OFFSET};
-    if (gpiod_line_config_add_line_settings(line_cfg, offsets, 1, settings) != 0)
-    {
-        fprintf(stderr, "Error adding line settings to config for pin %d: %s\n",
-                RST_PIN_OFFSET, strerror(errno));
-        gpiod_line_config_free(line_cfg);
-        gpiod_line_settings_free(settings);
-        gpiod_chip_close(chip_v2);
-        return -1;
-    }
-
-    // Create a request config object for the overall request
-    struct gpiod_request_config *req_cfg = gpiod_request_config_new();
-    if (!req_cfg)
-    {
-        perror("Error creating request config");
-        gpiod_line_config_free(line_cfg);
-        gpiod_line_settings_free(settings);
-        gpiod_chip_close(chip_v2);
-        return -1;
-    }
-    gpiod_request_config_set_consumer(req_cfg, "sx1255-reset");
-
-    // Request the lines using the 3-argument function signature
-    rst_line_request = gpiod_chip_request_lines(chip_v2, req_cfg, line_cfg);
-
-    // Free the config objects as they are no longer needed
-    gpiod_request_config_free(req_cfg);
-    gpiod_line_config_free(line_cfg);
-    gpiod_line_settings_free(settings);
-
-    if (!rst_line_request)
-    {
-        fprintf(stderr, "Error requesting GPIO lines for pin %d: %s\n",
-                RST_PIN_OFFSET, strerror(errno));
-        gpiod_chip_close(chip_v2);
-        return -1;
-    }
-    return 0;
-}
-
-int rst(uint8_t state)
-{
-    // Set value using gpiod V2 API
-    enum gpiod_line_value values[] = {state ? GPIOD_LINE_VALUE_ACTIVE : GPIOD_LINE_VALUE_INACTIVE};
-    if (gpiod_line_request_set_values(rst_line_request, values) < 0)
-    {
-        fprintf(stderr, "Error setting GPIO line value for pin %d to state %d: %s\n",
-                RST_PIN_OFFSET, state, strerror(errno));
-        return 1;
-    }
-    return 0;
-}
-
-void gpio_cleanup(void)
-{
-    if (rst_line_request)
-    {
-        gpiod_line_request_release(rst_line_request);
-    }
-    if (chip_v2)
-    {
-        gpiod_chip_close(chip_v2);
-    }
-}
 
 void print_help(const char *program_name)
 {
@@ -324,34 +50,19 @@ void print_help(const char *program_name)
     printf("  -h, --help                Display this help message and exit\n");
     printf("\n");
     printf("Example:\n");
-    printf("  %s -s 500 -r 433475000 -t 438000000\n", program_name);
+    printf("  %s -E -s 500 -r 433475000 -l 24 -p 24 -R 1 -P\n", program_name);
 }
 
 // --- Main Program Logic ---
 int main(int argc, char *argv[])
 {
-    mode = SPI_MODE_0;
-
-    if (spi_init((char *)spi_device) == 0)
+    if (sx1255_init(spi_device, gpio_chip_path, rst_pin_offset) != 0)
     {
-        // printf("SPI config OK\n");
-    }
-    else
-    {
+        fprintf(stderr, "Can not initialize device\nExiting\n");
         return -1;
     }
 
-    if (gpio_init() == 0)
-    {
-        ;
-    }
-    else
-    {
-        fprintf(stderr, "Can not initialize RST pin\nExiting\n");
-        return -1;
-    }
-
-    uint8_t val = sx1255_readreg(0x07);
+    uint8_t val = sx1255_read_reg(0x07);
     printf("Detected SX1255 chip version V%d%c", (val >> 4) & 0xF, 'A' + (val & 0xF) - 1);
     if (val == 0x11)
     {
@@ -404,13 +115,7 @@ int main(int argc, char *argv[])
         // reset
         case 'E':
             printf("Resetting device... ");
-            usleep(100000U);
-            if(rst(1)!=0)
-                return -1;
-            usleep(100000U);
-            if(rst(0)!=0)
-                return -1;
-            usleep(250000U);
+            sx1255_reset();
             printf("completed\n");
             break;
 
@@ -422,17 +127,17 @@ int main(int argc, char *argv[])
                 if (srate == 125)
                 {
                     printf("Setting sample rate to %d kSa/s\n", srate);
-                    rate = RATE_125K;
+                    rate = SX1255_RATE_125K;
                 }
                 else if (srate == 250)
                 {
                     printf("Setting sample rate to %d kSa/s\n", srate);
-                    rate = RATE_250K;
+                    rate = SX1255_RATE_250K;
                 }
                 else if (srate == 500)
                 {
                     printf("Setting sample rate to %d kSa/s\n", srate);
-                    rate = RATE_500K;
+                    rate = SX1255_RATE_500K;
                 }
                 else
                 {
@@ -442,15 +147,15 @@ int main(int argc, char *argv[])
             else
             {
                 printf("Invalid sample rate. Using 125 kHz.\n");
-                rate = RATE_125K;
+                rate = SX1255_RATE_125K;
             }
 
-            int8_t retval = sx1255_setrate(rate);
+            int8_t retval = sx1255_set_rate(rate);
             printf("I2S setup %s\n", retval == 0 ? "OK" : "error");
             if (retval != 0)
             {
                 printf("Exiting\n");
-                gpio_cleanup();
+                sx1255_cleanup();
                 return -1;
             }
             break;
@@ -537,10 +242,10 @@ int main(int argc, char *argv[])
                 printf("Missing LNA gain. Using 48 dB.\n");
                 lna_gain = 1;
             }
-            val = sx1255_readreg(0x0C);
+            val = sx1255_read_reg(0x0C);
             val &= (uint8_t)~(0xE0);
             val |= lna_gain << 5;
-            sx1255_writereg(0x0C, val);
+            sx1255_write_reg(0x0C, val);
             break;
 
         // pga gain
@@ -564,10 +269,10 @@ int main(int argc, char *argv[])
                 printf("Missing PGA gain. Using 30 dB.\n");
                 pga_gain = 15;
             }
-            val = sx1255_readreg(0x0C);
+            val = sx1255_read_reg(0x0C);
             val &= (uint8_t)~(0x1E);
             val |= pga_gain << 1;
-            sx1255_writereg(0x0C, val);
+            sx1255_write_reg(0x0C, val);
             break;
 
         // dac gain
@@ -606,10 +311,10 @@ int main(int argc, char *argv[])
                 printf("Missing DAC gain. Using -3 dB.\n");
                 dac_gain = 2;
             }
-            val = sx1255_readreg(0x08);
+            val = sx1255_read_reg(0x08);
             val &= (uint8_t)0x0F;
             val |= (uint8_t)dac_gain << 4;
-            sx1255_writereg(0x08, val);
+            sx1255_write_reg(0x08, val);
             break;
 
         // mixer gain
@@ -634,10 +339,10 @@ int main(int argc, char *argv[])
                 printf("Missing mixer gain. Using -9.5 dB.\n");
                 mix_gain = 0x0E;
             }
-            val = sx1255_readreg(0x08);
+            val = sx1255_read_reg(0x08);
             val &= (uint8_t)0xF0;
             val |= (uint8_t)mix_gain;
-            sx1255_writereg(0x08, val);
+            sx1255_write_reg(0x08, val);
             break;
 
         // rx pll bw
@@ -662,10 +367,10 @@ int main(int argc, char *argv[])
                 printf("Missing RX PLL bandwidth. Using 75 kHz.\n");
                 pll_bw = 0;
             }
-            val = sx1255_readreg(0x0E);
+            val = sx1255_read_reg(0x0E);
             val &= (uint8_t)~(0x06);
             val |= (uint8_t)pll_bw<<1;
-            sx1255_writereg(0x0E, val);
+            sx1255_write_reg(0x0E, val);
             break;
 
         // tx pll bw
@@ -690,10 +395,10 @@ int main(int argc, char *argv[])
                 printf("Missing TX PLL bandwidth. Using 75 kHz.\n");
                 pll_bw = 0;
             }
-            val = sx1255_readreg(0x0A);
+            val = sx1255_read_reg(0x0A);
             val &= (uint8_t)~(0x60);
             val |= (uint8_t)pll_bw<<5;
-            sx1255_writereg(0x0A, val);
+            sx1255_write_reg(0x0A, val);
             break;
 
         // enable/disable TX front end
@@ -703,16 +408,16 @@ int main(int argc, char *argv[])
                 val = atoi(optarg);
                 if (val == 0)
                 {
-                    val = sx1255_readreg(0x00);
+                    val = sx1255_read_reg(0x00);
                     val &= (uint8_t)~((1 << 2) | (1 << 3));
-                    sx1255_writereg(0x00, val);
+                    sx1255_write_reg(0x00, val);
                     printf("Disabling TX path.\n");
                 }
                 else
                 {
-                    val = sx1255_readreg(0x00);
+                    val = sx1255_read_reg(0x00);
                     val |= (uint8_t)(1 << 2) | (1 << 3);
-                    sx1255_writereg(0x00, val);
+                    sx1255_write_reg(0x00, val);
                     printf("Enabling TX path.\n");
                 }
             }
@@ -729,16 +434,16 @@ int main(int argc, char *argv[])
                 val = atoi(optarg);
                 if (val == 0)
                 {
-                    val = sx1255_readreg(0x00);
+                    val = sx1255_read_reg(0x00);
                     val &= (uint8_t)~(1 << 1);
-                    sx1255_writereg(0x00, val);
+                    sx1255_write_reg(0x00, val);
                     printf("Disabling RX path.\n");
                 }
                 else
                 {
-                    val = sx1255_readreg(0x00);
+                    val = sx1255_read_reg(0x00);
                     val |= (uint8_t)(1 << 1);
-                    sx1255_writereg(0x00, val);
+                    sx1255_write_reg(0x00, val);
                     printf("Enabling RX path.\n");
                 }
             }
@@ -750,7 +455,7 @@ int main(int argc, char *argv[])
 
         // get PLL lock flags
         case 'P':
-            val = sx1255_readreg(0x11);
+            val = sx1255_read_reg(0x11);
             printf("TX PLL %s\n", (val & (1 << 0)) ? "locked" : "unlocked");
             printf("RX PLL %s\n", (val & (1 << 1)) ? "locked" : "unlocked");
             break;
@@ -765,7 +470,7 @@ int main(int argc, char *argv[])
                     addr = atoi(optarg);
 
                 if (addr <= 0x13)
-                    printf("Register 0x%02X value: 0x%02X\n", addr, sx1255_readreg(addr));
+                    printf("Register 0x%02X value: 0x%02X\n", addr, sx1255_read_reg(addr));
                 else
                     printf("Register readout error: address out of range.\n");
             }
@@ -785,7 +490,7 @@ int main(int argc, char *argv[])
                 if (addr <= 0x13)
                 {
                     printf("Seting register 0x%02X to 0x%02X\n", addr, val);
-                    sx1255_writereg(addr, val);
+                    sx1255_write_reg(addr, val);
                 }
                 else
                     printf("Register write error: address out of range.\n");
@@ -799,17 +504,12 @@ int main(int argc, char *argv[])
         // help
         case 'h':
             print_help(argv[0]);
-            gpio_cleanup();
+            sx1255_cleanup();
             return 0;
             break;
         }
     }
 
-    // we leave some registers for later
-    // TODO: add those to args above as well
-    sx1255_writereg(0x0D, (0x01 << 5) | (0x05 << 2) | 0x03);
-    sx1255_writereg(0x0B, 5);
-
-    gpio_cleanup();
+    sx1255_cleanup();
     return 0;
 }
