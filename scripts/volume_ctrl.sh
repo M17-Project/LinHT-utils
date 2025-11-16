723d0e306a11eede8c0d226c6c0681841ca14d8e
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2025-11-16 20:40:20 +0100

    Adjust volume range in volume_ctrl.sh

--- scripts/volume_ctrl.sh
+++ scripts/volume_ctrl.sh
@@ -5,7 +5,7 @@ b=100 #log base
 while true
 do
     vol=$(< /sys/bus/iio/devices/iio:device0/in_voltage0_raw)
-    vol=$(echo "170 + l(1+($vol/3000)*($b-1))/l($b) * (255-170) + 1" | bc -l)
+    vol=$(echo "100 + l(1+($vol/3000)*($b-1))/l($b) * (255-100) + 1" | bc -l)
     vol=$(printf "%.0f" "$vol")
     amixer -q -c wm8960audio set Playback $vol
     #echo $vol
