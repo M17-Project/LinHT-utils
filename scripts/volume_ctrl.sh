fcb890a5c98a941c02a9200ab49238bb177eef17
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2025-12-04 15:27:25 +0100

    improved the volume control script

--- scripts/volume_ctrl.sh
+++ scripts/volume_ctrl.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 b=5000 #log base
+last_vol=-1
 
-while true
-do
-    vol=$(< /sys/bus/iio/devices/iio:device0/in_voltage0_raw)
-    vol=$(echo "100 + l(1+($vol/3000)*($b-1))/l($b) * (255-100) + 1" | bc -l)
-    vol=$(printf "%.0f" "$vol")
-    amixer -q -c wm8960audio set Playback $vol
-    #echo $vol
+while true; do
+    raw=$(< /sys/bus/iio/devices/iio:device0/in_voltage0_raw)
+
+    #compute with awk instead of bc
+    vol=$(awk -v v="$raw" -v b="$b" '
+        BEGIN {
+            out = 100 + log(1+(v/3000)*(b-1))/log(b) * (255-100) + 1
+            printf "%d", out
+        }')
+
+    #only apply change if necessary
+    if [ "$vol" -ne "$last_vol" ]; then
+        amixer -q -c wm8960audio set Playback "$vol"
+        last_vol=$vol
+    fi
+
     sleep 0.2
 done
